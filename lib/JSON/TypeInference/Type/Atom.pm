46244f3f58509361b9dccd006e016066058541e3
aereal <aereal@aereal.org>
2015-10-17 01:48:16 +0900

    + docs about Type::Atom
    [ci skip]

--- lib/JSON/TypeInference/Type/Atom.pm
+++ lib/JSON/TypeInference/Type/Atom.pm
@@ -13,3 +13,23 @@ sub signature {
 }
 
 1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+JSON::TypeInference::Type::Atom - Base class for JSON value types
+
+=head1 DESCRIPTION
+
+C< JSON::TypeInference::Type::Atom > is a base class for JSON value type and provides some default implementations.
+
+It also introduces a convention that the inherited type's signature is its name.
+
+=head1 AUTHOR
+
+aereal E<lt>aereal@aereal.orgE<gt>
+
+=cut
+
